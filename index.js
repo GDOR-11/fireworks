63ffd3155f1260927cb6140915a4d43d44f12089
GDOR-11 <77753977+GDOR-11@users.noreply.github.com>
2021-12-31 12:26:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+import {autoResizeCanvas} from './autoResizeCanvas.js';
+import {Firework} from './fireWork.js';
+
+function createFirework() {
+	let x = Math.random() * screenWidth;
+	let maxvy = Math.sqrt(2 * gy * screenHeight) - 10;
+	let minvy = 100;
+	let vy = -(Math.random() * (maxvy - minvy) + minvy);
+	let color = {
+		r: Math.random() * 255,
+		g: Math.random() * 255,
+		b: Math.random() * 255,
+		a: 255
+	};
+	fireworks.push(new Firework(x, screenHeight, 0, vy, color));
+}
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+autoResizeCanvas(canvas);
+
+const gx = 0;
+const gy = 50;
+let fireworks = [];
+
+const dt = 1 / 60;
+function render() {
+	let imgdata = ctx.createImageData(screenWidth, screenHeight);
+	let data = imgdata.data;
+
+	if(Math.random() < 0.5) {
+		createFirework();
+	}
+
+	for(let i = fireworks.length - 1;i >= 0;i--) {
+		fireworks[i].update(gx, gy, dt);
+		fireworks[i].render(data, screenWidth, screenHeight);
+		if(fireworks[i].sparks.length == 0) {
+			fireworks.splice(i, 1);
+		}
+	}
+
+
+	ctx.putImageData(imgdata, 0, 0);
+	requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
